7e777ecd9b29bbe42938111b10383584712936ed
Rizzilient <105723670+Rizzilient@users.noreply.github.com>
2024-05-13 22:19:52 -0400

    changesw

--- Assets/Scripts/Orange IT Script.cs
+++ Assets/Scripts/Orange IT Script.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class OrangeITScript : MonoBehaviour
+public class EnemyAI2D : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float attackRange = 3f;
+    public float moveSpeed = 5f;
+
+    private Transform player;
+    private bool isPlayerInRange;
+    private Animator animator;
+
     void Start()
     {
-        
+        // Find the player object by tag
+        player = GameObject.FindGameObjectWithTag("Richard").transform;
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Check if player is in range
+        float distanceToPlayer = Mathf.Abs(transform.position.x - player.position.x);
+        if (distanceToPlayer <= attackRange)
+        {
+            isPlayerInRange = true;
+            AttackPlayer();
+        }
+        else
+        {
+            isPlayerInRange = false;
+        }
+
+        // Update animator parameters based on movement
+        animator.SetBool("isWalking", !isPlayerInRange);
+        animator.SetBool("isIdle", isPlayerInRange);
+
+        // If player is not in range, move towards player
+        if (!isPlayerInRange)
+        {
+            MoveTowardsPlayer();
+        }
+    }
+
+    void MoveTowardsPlayer()
+    {
+        // Move towards the player's horizontal position
+        float direction = Mathf.Sign(player.position.x - transform.position.x);
+        transform.Translate(Vector2.right * direction * moveSpeed * Time.deltaTime);
+    }
+
+    void AttackPlayer()
+    {
+        // Implement attack behavior here, such as reducing player health
+        Debug.Log("Attacking Player!");
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,149 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using System;
+
+//public class PlayerController : MonoBehaviour
+//{
+
+//    public float speed;
+//    public Rigidbody2D rb;
+//    public float jumpForce;
+//    public bool jump;
+//    private bool facingRight;
+//    private bool isWalking;
+//    public Transform groundCheck;
+//    private Animator anim;
+//    public LayerMask groundLayers;
+//    public bool isGrounded;
+
+//    private float horizontalMotion, verticalMotion;
+
+//    public static Action<float> OnUpgradeApply;
+
+//    private void OnEnable()
+//    {
+
+//        OnUpgradeApply += upgradeApply;
+//    }
+
+//    private void OnDisable()
+//    {
+
+//        OnUpgradeApply -= upgradeApply;
+//    }
+
+
+//    // Use this for initialization
+//    void Start()
+//    {
+//        rb = GetComponent<Rigidbody2D>();
+//        jump = false;
+//        facingRight = true;
+//        anim = GetComponent<Animator>();
+
+//        DontDestroyOnLoad(this.gameObject);
+//    }
+
+//    // Update is called once per frame - used for button/key presses
+//    void Update()
+//    {
+//        horizontalMotion = Input.GetAxis("Horizontal");    //get the horizontal motion, if any
+//        verticalMotion = Input.GetAxis("Vertical");        //get the vertical motion, if any
+//        if (Input.GetKeyDown(KeyCode.Space))
+//        {
+//            jump = true;
+//        }
+//    }
+
+//    void FixedUpdate()
+//    {
+//        if (jump && isGrounded)   //space is pressed and the player isn't already jumping and the player is grounded
+//        {
+//            anim.SetBool("IsGrounded", false);
+//            rb.AddForce(new Vector2(0, jumpForce));
+//            //add the force to have the player jump            
+//        }
+//        else if (!isGrounded)
+//        {
+//            jump = false;
+//            anim.SetBool("IsGrounded", true);
+//        }
+//        if (horizontalMotion > 0f || horizontalMotion < 0f)  //if the player is walking
+//        {
+//            anim.SetBool("isIdle", false);       //control the animation - change the boolean in the animator
+//        }
+//        else
+//        {
+//            anim.SetBool("isIdle", true);
+//        }
+//        //the following is for flipping the player sprite so it is always facing the right direction
+//        if (horizontalMotion < 0 && facingRight) //need to change direction
+//        {
+//            Flip();
+//        }
+//        else if (horizontalMotion > 0 && !facingRight)
+//        {
+//            Flip();
+//        }
+//        Vector3 motion = new Vector3(speed * horizontalMotion * Time.deltaTime, 0, 0); //standard for vector motion (this is only for x axis)
+//        this.transform.position = this.transform.position + motion;
+//        isGrounded = Physics2D.OverlapCircle(groundCheck.position, .05f, groundLayers);
+//    }
+
+//    void Flip()
+//    {
+//        facingRight = !facingRight;
+//        Vector3 playerScale = this.transform.localScale;
+//        playerScale.x *= -1;
+//        this.transform.localScale = playerScale;
+//    }
+
+//    // Power up code goes here to change player stats.
+//    void upgradeApply(float f) {
+
+//        switch (f) {
+
+//            case 1:
+//                Debug.Log(f);
+//                break;
+//            case 2:
+//                Debug.Log(f);
+//                break;
+//            case 3:
+//                Debug.Log(f);
+//                break;
+//            case 4:
+//                Debug.Log(f);
+//                break;
+//            case 5:
+//                Debug.Log(f);
+//                break;
+//            case 6:
+//                Debug.Log(f);
+//                break;
+//            case 7:
+//                Debug.Log(f);
+//                break;
+//            case 8:
+//                Debug.Log(f);
+//                break;
+//            case 9:
+//                Debug.Log(f);
+//                break;
+//            case 10:
+//                Debug.Log(f);
+//                break;
+//            default:
+//                Debug.Log("Nothing Happens");
+//                break;
+
+//        }
+
+//    }
+
+//}
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -69,7 +215,9 @@ public class PlayerController : MonoBehaviour
             jump = false;
             anim.SetBool("IsGrounded", true);
         }
-        if (horizontalMotion > 0f || horizontalMotion < 0f)  //if the player is walking
+
+        // Update walking animation
+        if (Mathf.Abs(horizontalMotion) > 0f) //if the player is walking
         {
             anim.SetBool("isIdle", false);       //control the animation - change the boolean in the animator
         }
@@ -77,6 +225,17 @@ public class PlayerController : MonoBehaviour
         {
             anim.SetBool("isIdle", true);
         }
+
+        // Update running animation
+        if (Mathf.Abs(horizontalMotion) > 0f) //if the player is moving
+        {
+            anim.SetBool("isRunning", true);       //control the animation - change the boolean in the animator
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
         //the following is for flipping the player sprite so it is always facing the right direction
         if (horizontalMotion < 0 && facingRight) //need to change direction
         {
@@ -100,9 +259,11 @@ public class PlayerController : MonoBehaviour
     }
 
     // Power up code goes here to change player stats.
-    void upgradeApply(float f) {
+    void upgradeApply(float f)
+    {
 
-        switch (f) {
+        switch (f)
+        {
 
             case 1:
                 Debug.Log(f);
@@ -139,7 +300,7 @@ public class PlayerController : MonoBehaviour
                 break;
 
         }
-    
+
     }
 
 }
